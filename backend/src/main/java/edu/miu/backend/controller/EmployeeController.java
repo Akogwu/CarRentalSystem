a4ced596d22c0b9b574c3870cf72a58606203f5f
Abdessamad-Jadid <ajadid@miu.edu>
2022-02-01 20:18:42 -0600

    add EmployeeController , Repository and Service

---
+++ backend/src/main/java/edu/miu/backend/controller/EmployeeController.java
@@ -0,0 +1,29 @@
+package edu.miu.backend.controller;
+
+import edu.miu.backend.model.Employee;
+import edu.miu.backend.services.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    @Autowired
+    public void setEmployeeService(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Employee>> getAllEmployees() {
+        return ResponseEntity.ok(employeeService.findAll());
+    }
+
+}
---
+++ backend/src/main/java/edu/miu/backend/services/impl/EmployeeServiceImpl.java
@@ -0,0 +1,27 @@
+package edu.miu.backend.services.impl;
+
+import edu.miu.backend.model.Car;
+import edu.miu.backend.model.Employee;
+import edu.miu.backend.repository.EmployeeRepository;
+import edu.miu.backend.services.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public abstract  class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    public void setEmployeeRepository(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public List<Employee> findAll() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee findById(Long id) {
+        return employeeRepository.findById(id).orElse(null);
+    }
+}
