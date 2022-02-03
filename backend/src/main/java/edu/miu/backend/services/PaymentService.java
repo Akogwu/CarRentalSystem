0ae5baa9ca7d9c29aa016c7154cc8b282f501ba1
BiniamGirmay <bgirmay@miu.edu>
2022-02-02 19:11:46 -0600

    payment

---
+++ backend/src/main/java/edu/miu/backend/services/PaymentService.java
@@ -0,0 +1,12 @@
+package edu.miu.backend.services;
+
+import edu.miu.backend.model.Payment;
+
+import java.util.List;
+
+public interface PaymentService {
+    public List<Payment> findAll();
+    public Payment save(Payment payment);
+    public Payment findById(Long id) throws Exception;
+    public void delete(Long id) throws Exception;
+}
